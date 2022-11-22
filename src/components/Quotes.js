a83ecc798ff37fac3f3f91b04a6e907cc1234522
gandradep <eventsonido@gmail.com>
2022-11-22 10:26:38 +0100

    Add link to go home

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Quotes = (props) => {
     const { slug }= useParams();
     const authors = useSelector((state) => state.authors);    
-    const authorInfo = authors.filter((el)=> el.slug === slug);
+    const authorInfo = authors.filter((el)=> el.slug === slug)[0];
     console.log(authorInfo);
     return(  
         
             <div >
-                <h2>{authorInfo[0].name}</h2>
+                <header>
+                    <NavLink to="/">
+                        <h3>&lt;</h3>
+                    </NavLink>
+                    
+                    <h2>{authorInfo.name}</h2>                    
+                </header>
+                <p>{authorInfo.description}</p>
             </div>
         
     );}
