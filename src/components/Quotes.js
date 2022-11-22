3cee15934d56371c11e0f87b3bfe5b8a955a3636
gandradep <eventsonido@gmail.com>
2022-11-22 11:28:39 +0100

    Display quotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getQuotes } from '../redux/quotes/quotes';
 import { NavLink } from 'react-router-dom';
 
 const Quotes = (props) => {
     const { slug }= useParams();
-    const authors = useSelector((state) => state.authors);    
+    const dispatch = useDispatch();
+    const authors = useSelector((state) => state.authors);  
+    const quotes = useSelector((state) => state.quotes);  
     const authorInfo = authors.filter((el)=> el.slug === slug)[0];
-    console.log(authorInfo);
+    const objThunk = {
+        slug,
+        quotes: authorInfo.quotes,
+    };
+    
+
+    useEffect(() => {
+        dispatch(getQuotes(objThunk));           
+    }, [dispatch]);    
     return(  
         
             <div >
@@ -19,6 +30,11 @@ const Quotes = (props) => {
                     <h2>{authorInfo.name}</h2>                    
                 </header>
                 <p>{authorInfo.description}</p>
+                <ul>
+                    {quotes?.map((item) => (
+                        <li key={item.id}>{item.content}</li>
+                    ))}
+                </ul>
             </div>
         
     );}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authorReducer from './homepage/homepage';
+import quotesReducer from './quotes/quotes';
 
 export const store = configureStore({
   reducer: {
     authors: authorReducer,
+    quotes: quotesReducer,
   },
 });
